6cff3162d7fd53fa73cd4af4b11c019ed8ad4be0
Niranga De Silva <nirangad@99x.io>
2025-02-10 14:09:21 +0530

    Remove files ignored by .gitignore

--- GameStore.Api/Models/Contracts/GameContract.cs
+++
@@ -1,3 +0,0 @@
-namespace GameStore.Api.Models.Contracts;
-
-public record class GameContract(int Id, string Name, string Genre, decimal Price, DateOnly? ReleasedDate, string? Url);
--- GameStore.Api/Models/Entities/Genre.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace GameStore.Api.Models.Entities;
-
-public class Genre
-{
-    public int Id { get; set;}
-    public required string Name { get; set;}
-}
